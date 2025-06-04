f91741852918a657607ae8ba9ad33a2d4fed484e
Tejashwa2810 <tejashwa2810@gmail.com>
2025-06-04 11:54:52 +0530

    Added SecurityFilterChain

--- src/main/java/com/example/bcryptauthservice/Configuration/BCryptPasswordEncoderConfig.java
+++ src/main/java/com/example/bcryptauthservice/Configuration/BCryptPasswordEncoderConfig.java
@@ -3,6 +3,8 @@ package com.example.bcryptauthservice.Configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class BCryptPasswordEncoderConfig {
@@ -11,4 +13,13 @@ public class BCryptPasswordEncoderConfig {
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.cors().disable();
+        httpSecurity.csrf().disable();
+        httpSecurity.authorizeHttpRequests(authorize-> authorize.anyRequest().permitAll());
+        return httpSecurity.build();
+    }
+
 }
